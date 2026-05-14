523c98830a31115f9cafbfce2e9199ae9b77a674
Gxstavo-dev <gps5821s@outlook.com>
2026-05-14 17:33:50 -0600

    Creacion de layouts para index.tsx hero.tsx -> seccion principal navbar.tsx -> seccion donde estara los links de navegacion

--- src/components/layout/index/hero.tsx
+++ src/components/layout/index/hero.tsx
@@ -1,1 +1,8 @@
-export default function Hero() {}
+// primera seccion de la landing page
+export default function Hero() {
+  return (
+    <div className="w-full h-screen flex flex-col relative">
+      <div className="w-full h-full flex flex-col items-center p-11 gap-7"></div>
+    </div>
+  );
+}
--- src/components/layout/index/navbar.tsx
+++ src/components/layout/index/navbar.tsx
@@ -1,11 +1,12 @@
-import { useState, useRef } from "react";
-import type { MouseEvent } from "react";
+import { useState, useRef } from "react"; // para interactuar con los estados del dom
+import type { MouseEvent } from "react"; // manejo de eventos
 
 export default function Navbar() {
-  const links: string[] = ["Home", "Login", "Registro"]; // para manejarlo de manera mas sencilla de editarlo
-  const [visible, setVisible] = useState(false);
-  const [style, setStyle] = useState({ left: 0, top: 0, width: 0, height: 0 });
-  const navbar = useRef<HTMLUListElement>(null);
+  const links: string[] = ["Inicio", "Login", "Registro"]; // para manejarlo de manera mas sencilla de editarlo
+  const [visible, setVisible] = useState(false); // de manera determinada iniciara en falso
+  const [style, setStyle] = useState({ left: 0, top: 0, width: 0, height: 0 }); // para agregar estilos para el span que tendra posicion absoluta para que siga el mouse entre los links
+  const navbar = useRef<HTMLUListElement>(null); // los elementos li dentro de la lista ul
+  const [contenido, setContenido] = useState("");
 
   // funcion para saber en donde esta el mouse dentro del nav
   const eventoMouse = (e: MouseEvent<HTMLLIElement>) => {
@@ -31,9 +32,13 @@ export default function Navbar() {
       <ul
         ref={navbar}
         className="w-3xs h-full flex justify-center items-center gap-6 relative"
-        onMouseLeave={() => setVisible(false)} // si se sale del nav lo detecte de nuevo como null
+        onMouseLeave={() => {
+          setVisible(false);
+          setContenido("");
+        }} // si se sale del nav lo detecte de nuevo como null
       >
         {visible && (
+          // estilos para el span si es que es true el estado
           <span
             className="absolute inset-y-1 z-0 rounded-md bg-slate-300/30  transition-all duration-200 ease-out"
             style={{
@@ -52,6 +57,26 @@ export default function Navbar() {
               key={index} // clave unica
               className="text-[12px] cursor-pointer w-24 bg-transparent z-10 text-center p-0.75"
               onMouseEnter={eventoMouse} // le pasamos el indice activo
+              onClick={(e: MouseEvent) => {
+                const presionado = e.currentTarget; // obtener el li presionado
+                const texto = presionado.textContent; // obtener el texto de ese li
+
+                // dependiendo del contenido es a donde se va a redireccionar al usuario
+                switch (texto) {
+                  case "Inicio":
+                    window.location.href = "/";
+                    break;
+                  case "Login":
+                    window.location.href = "/login";
+                    break;
+                  case "Registro":
+                    window.location.href = "/registro";
+                    break;
+                  default:
+                    console.log("Indice no existe");
+                    break;
+                }
+              }}
             >
               {link}
             </li>
